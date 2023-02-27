fdd7a698243e680fd2cc662a82aa6e16cf3c8ad8
pavelkushtseika <pavelkushtseika@outlook.com>
2023-02-27 14:22:38 -0800

    Modify header for toggle-sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,9 +26,11 @@ import { SidebarComponent } from './components/layout/sidebar/sidebar.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatIconModule,
     RouterModule.forRoot(AppRoutes)
   ],
   providers: [],

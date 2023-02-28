098e9019d34f75bd09b95ae699f933f87f7ab174
pavelkushtseika <pavelkushtseika@outlook.com>
2023-02-28 05:25:46 -0800

    Pagination for Gallery is done

--- src/app/components/pages/gallery/components/gallery/gallery.component.ts
+++ src/app/components/pages/gallery/components/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 
@@ -14,27 +16,42 @@ const initialGallery: ImageInfo[] = [];
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
   gallery: ImageInfo[] = initialGallery;
-  private readonly maxCardWidth: number = 250;
+  dataSource = new MatTableDataSource<ImageInfo>(initialGallery);
 
   isLoading$: Observable<boolean>;
   error$: Observable<string | null>;
   gallery$: Observable<ImageInfo[]>;
+  displayGallery: Observable<ImageInfo[]> = new Observable<ImageInfo[]>();
 
   breakpoint: number = 3;
 
-  constructor(private store: Store<AppState>) {
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
+  constructor(private store: Store<AppState>, private changeDetectorRef: ChangeDetectorRef) {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.gallery$ = this.store.pipe(select(gallerySelector));
-  }
 
-  onResize(_event: any) {
-    this.breakpoint = Math.floor(window.innerWidth / this.maxCardWidth);
+    this.gallery$.subscribe(gallery => this.dataSource.data = gallery);
+  }
+  
+  onResize(_event?: any) {
+    this.breakpoint = window.innerWidth <= 600 ? 2 : Math.floor(window.innerWidth / 500) * 2;
   }
 
   ngOnInit(): void {
+    this.onResize();
+    this.changeDetectorRef.detectChanges();
     this.store.dispatch(GalleryActions.getGallery());
+    this.dataSource.paginator = this.paginator;
+    this.displayGallery = this.dataSource.connect();
+  }
+
+  ngOnDestroy() {
+    if (this.dataSource) { 
+      this.dataSource.disconnect(); 
+    }
   }
 }

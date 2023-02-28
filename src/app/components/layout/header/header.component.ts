80977848714fa1011f904533e285645bfdda9d22
pavelkushtseika <pavelkushtseika@outlook.com>
2023-02-27 16:08:18 -0800

    Styled header and use best practices

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouteInfo, Routes } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuItems: RouteInfo[] = [];
 
+  ngOnInit() {
+    this.menuItems = Routes;
+  }
 }

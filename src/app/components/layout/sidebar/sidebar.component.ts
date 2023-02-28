e7f3367a872e5f4084f66b3e478e63d337ebcb02
pavelkushtseika <pavelkushtseika@outlook.com>
2023-02-27 16:01:51 -0800

    Styled sidebar and use best practices

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface RouteInfo {
+  path: string,
+  title: string,
+  icon: string,
+  class: string
+}
+
+export const Routes: RouteInfo[] = [
+  { path: '/home',      title: 'Home',      icon: 'home',        class: '' },
+  { path: '/posts',     title: 'Posts',     icon: 'view_list',     class: '' },
+  { path: '/gallery',   title: 'Gallery',   icon: 'image',       class: '' },
+]
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public menuItems: RouteInfo[] = [];
 
+  ngOnInit() {
+    this.menuItems = Routes;
+  }
 }
